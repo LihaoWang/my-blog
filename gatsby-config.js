93986fe53050f12f521a658b82d18763025be883
leowang <leowang9863@gmail.com>
2021-03-11 18:11:09 -0500

    add tools

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,10 @@ module.exports = {
             title: `About`,
             slug: `/about`,
           },
+          {
+            title: `Tools`,
+            slug: `/tools`,
+          },
         ],
         externalLinks: [
           {
